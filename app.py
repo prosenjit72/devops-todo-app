ae9b0a24779e36b7767b7daa5a55920ce2ab1f71
Prosenjit Das <prosenjitdas725503@gmail.com>
2026-05-17 21:59:19 +0600

    feat: add docker-compose with PostgreSQL

--- app.py
+++ app.py
@@ -48,7 +48,8 @@ def delete(id):
     db.session.commit()
     return redirect('/')
 
+with app.app_context():
+    db.create_all()
+
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(host='0.0.0.0', port=5000, debug=True)
